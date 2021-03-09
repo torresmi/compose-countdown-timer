b3dbf29881e2518d1919ce3d5f58e381cf32eee3
Micahel Torres <torresmi@mail.gvsu.edu>
2021-03-09 03:04:25 -0500

    animate toast and friends

--- app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
@@ -1,10 +1,35 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.home
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -16,16 +41,27 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material.icons.filled.Timer
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.amber100
+import com.example.androiddevchallenge.ui.theme.toastBrown
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+
+private const val TIME_FORMAT = "ss"
 
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel = HomeViewModel()) {
@@ -35,8 +71,6 @@ fun HomeScreen(homeViewModel: HomeViewModel = HomeViewModel()) {
         onTimeCancel = homeViewModel::end,
     )
 }
-
-private const val TIME_FORMAT = "m:s"
 
 @Composable
 fun Home(
@@ -49,7 +83,7 @@ fun Home(
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.home_title)) },
                 elevation = 4.dp,
-                backgroundColor = MaterialTheme.colors.primary
+                backgroundColor = MaterialTheme.colors.primary,
             )
         },
         floatingActionButton = {
@@ -75,18 +109,74 @@ fun Home(
 private fun TimerDisplay(timer: HomeViewModel.Timer) {
     when (timer) {
         is HomeViewModel.Timer.Running -> {
-            val remainingTime = SimpleDateFormat(TIME_FORMAT, Locale.getDefault())
-                .format(Date(timer.remainingTimeMillis))
-            val text = stringResource(id = R.string.home_timer, remainingTime)
-
-            Text(text = text, style = MaterialTheme.typography.h1)
+            TimerRun(timer)
         }
         is HomeViewModel.Timer.Stopped -> {
-            val text = stringResource(id = R.string.home_start)
-
-            Text(text = text, style = MaterialTheme.typography.h4)
+            TimerStop()
+        }
+        is HomeViewModel.Timer.Finished -> {
+            TimerFinish()
         }
     }
+}
+
+@Composable
+private fun TimerRun(timer: HomeViewModel.Timer.Running) {
+    val remainingTime = SimpleDateFormat(TIME_FORMAT, Locale.getDefault())
+        .format(Date(timer.remainingTimeMillis))
+    val text = stringResource(id = R.string.home_timer, remainingTime)
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = text, style = MaterialTheme.typography.h1)
+
+        val breadColor = remember { Animatable(amber100) }
+        LaunchedEffect(key1 = timer.remainingTimeMillis) {
+            breadColor.animateTo(
+                toastBrown,
+                animationSpec = tween(
+                    durationMillis = timer.remainingTimeMillis.toInt(),
+                    easing = LinearEasing,
+                ),
+            )
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_toast),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(breadColor.value, BlendMode.DstAtop),
+            modifier = Modifier.width(235.dp),
+        )
+    }
+}
+
+@Composable
+private fun TimerStop() {
+    val text = stringResource(id = R.string.home_start)
+
+    Text(text = text, style = MaterialTheme.typography.h4)
+}
+
+@Composable
+private fun TimerFinish() {
+    val infiniteTransition = rememberInfiniteTransition()
+    val jump by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 20.dp.value,
+        animationSpec = infiniteRepeatable(
+            animation = tween(750),
+            repeatMode = RepeatMode.Reverse,
+        ),
+    )
+
+    Image(
+        painter = painterResource(id = R.drawable.finished),
+        contentDescription = null,
+        modifier = Modifier.offset(y = jump.dp),
+    )
 }
 
 @Composable
@@ -97,7 +187,8 @@ private fun TimerFAB(
 ) {
     val (icon, action) = when (timer) {
         is HomeViewModel.Timer.Running -> Icons.Default.Stop to onTimeCancel
-        is HomeViewModel.Timer.Stopped -> Icons.Default.Timer to onTimeStart
+        is HomeViewModel.Timer.Stopped,
+        is HomeViewModel.Timer.Finished -> Icons.Default.Timer to onTimeStart
     }
 
     FloatingActionButton(onClick = action) {
--- app/src/main/java/com/example/androiddevchallenge/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/HomeViewModel.kt
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.home
 
 import android.os.CountDownTimer
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import java.util.concurrent.TimeUnit
 
@@ -20,13 +35,14 @@ class HomeViewModel : ViewModel() {
 
     fun start() {
         currentTimer?.cancel()
-        currentTimer = object : CountDownTimer(TOTAL_TIME_MILLIS, INTERVAL_TIME_MILLIS) {
+        currentTimer = object : CountDownTimer(TOTAL_TOAST_TIME_MILLIS, INTERVAL_TIME_MILLIS) {
             override fun onTick(millisUntilFinished: Long) {
                 viewState = viewState.copy(timer = Timer.Running(millisUntilFinished))
             }
 
             override fun onFinish() {
-                end()
+                viewState = viewState.copy(timer = Timer.Finished)
+                currentTimer = null
             }
         }.start()
     }
@@ -42,10 +58,11 @@ class HomeViewModel : ViewModel() {
     }
 
     sealed class Timer {
-        data class Running(val remainingTimeMillis: Long) : Timer()
+        class Running(val remainingTimeMillis: Long) : Timer()
         object Stopped : Timer()
+        object Finished : Timer()
     }
 }
 
-private val TOTAL_TIME_MILLIS: Long = TimeUnit.MINUTES.toMillis(1)
+private val TOTAL_TOAST_TIME_MILLIS: Long = TimeUnit.SECONDS.toMillis(6)
 private val INTERVAL_TIME_MILLIS: Long = TimeUnit.SECONDS.toMillis(1)
